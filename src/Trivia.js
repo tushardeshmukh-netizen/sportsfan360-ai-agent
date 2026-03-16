74f78b1435896e4ca6a4e3007e6d40286e3570a4
Tushar Deshmukh <tushar.deshmukh@sportsfan360.com>
2026-03-16 17:00:32 +0530

    Fix Trivia eslint error

--- src/Trivia.js
+++ src/Trivia.js
@@ -1,4 +1,4 @@
-import React,{useState,useEffect} from "react";
+import React,{useState} from "react";
 import confetti from "canvas-confetti";
 
 const API_URL="https://sportsfan360-ai-agent-1.onrender.com";
